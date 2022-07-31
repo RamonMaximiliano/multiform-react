9d0198a689dbd0bf3ce06da01fa3804a32cd60d4
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-07-31 12:39:08 -0300

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -24,3 +24,31 @@ function App() {
 }
 
 export default App;
+
+
+
+/*
+
+Pagina 1: 
+colocar nome nos dados 
+botão next
+lateral aparece que step vc esta
+
+Pagina 2:
+aparece nome colocado na pagina 1 
+se voltar na pagina 1 e trocar, na pagina 2 o nome atual aparece
+botão next e botão voltar
+lateral aparece que step vc esta
+escolhe se iniciante ou não
+
+Pagina 3: 
+aparece nome colocado na pagina 1 
+add email e github
+botão next e botão voltar
+lateral aparece que step vc esta
+
+Pagina 4:
+display dos dados coletados nas ultimas paginas / se voltar e trocar os dados, os mesmos são atualizados na ultima
+lateral aparece que step vc esta
+
+*/
