cd86baed618432a618595562a3c7127837245744
Ramon <ramon_max1986@hotmail.com>
2022-08-09 13:49:59 -0300

    add classes

--- src/components/Page2/index.tsx
+++ src/components/Page2/index.tsx
@@ -1,20 +1,28 @@
 import './styles.css'
 import { useNavigate } from 'react-router-dom'
 import { LoginContext } from '../../Context/Context'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 export const Page2 = () => {
+    const [level1div, setActiveLevel1] = useState<boolean>()
+    const [level2div, setActiveLevel2] = useState<boolean>()
     const navigate = useNavigate();
 
     const { FullProfile, setPage, setProfessionalLevel } = useContext(LoginContext)
 
     function level1() {
+        setActiveLevel1(true)
+        setActiveLevel2(false)
         setProfessionalLevel(1)
     }
+    let toogleLevel1 = level1div ? "active":"";
 
     function level2() {
+        setActiveLevel2(true)
+        setActiveLevel1(false)
         setProfessionalLevel(2)
     }
+    let toogleLevel2 = level2div ? "active":"";
 
     function backPage() {
         setProfessionalLevel('')
@@ -40,14 +48,14 @@ export const Page2 = () => {
             </div>
 
             <div className="page2-options">
-                <div className="sub-page2-options" onClick={level1}>
+                <div className={`sub-page2-options ${toogleLevel1}`} onClick={level1} id="level1">
                     <div className="page2-icon">👶</div>
                     <div>
                         <h3>Sou iniciante</h3>
                         <p>Comecei a programar há menos de 2 anos</p>
                     </div>
                 </div>
-                <div className="sub-page2-options" onClick={level2}>
+                <div className={`sub-page2-options ${toogleLevel2}`} onClick={level2}>
                     <div className="page2-icon">😎</div>
                     <div>
                         <h3>Sou Programador</h3>
